9e52d4013bc99c1abfb97055591909c45e00d544
Caroline SENES <caro67200@gmail.com>
2022-03-10 16:25:32 +0100

    add loading spinner for Products component

---
+++ src/components/Loading.svelte
@@ -0,0 +1,6 @@
+<div class="loading">
+    <h1>Ca arrive ...</h1>
+    <p>Chargement des données d'Heroku</p>
+    <img src="../../public/assets/images/loading.gif" alt="gif de chargement">
+</div>
+
