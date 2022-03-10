195252dcb62a2f9def7893d4616104900e52cb5f
Caroline SENES <caro67200@gmail.com>
2022-03-10 21:42:36 +0100

    add constants/links for big navbar menu

--- src/components/Navbar/BigNavbar.svelte
+++ src/components/Navbar/BigNavbar.svelte
@@ -1,16 +1,24 @@
 <script>
-    import CartButton from '../Cart/CartButton.svelte'
-    import TitleNavbar from '../Navbar/TitleNavBar.svelte'
+  import CartButton from "../Cart/CartButton.svelte";
+  import TitleNavbar from "../Navbar/TitleNavBar.svelte";
+  import links from "../../constants/links";
+  import { link } from "svelte-routing";
 </script>
 
 <nav class="navbar">
-    <div class="nav-container">
-        <div class="nav-center">
-            <p>nav links</p>
-            <TitleNavbar />
-            <div class="nav-aside">
-                <CartButton />
-            </div>
-        </div>
+  <div class="nav-container">
+    <div class="nav-center">
+      <ul class="nav-links">
+        {#each links as navLink}
+          <li>
+            <a href={navLink.url} use:link>{navLink.text}</a>
+          </li>
+        {/each}
+      </ul>
+      <TitleNavbar />
+      <div class="nav-aside">
+        <CartButton />
+      </div>
     </div>
+  </div>
 </nav>
---
+++ src/constants/links.js
@@ -0,0 +1,6 @@
+//permet de répertorier les url de la navbar au même endroit
+export default[
+    {text:'home',url:'/'},
+    {text:'products',url:'/products'},
+    {text:'about',url:'/about'},
+]
